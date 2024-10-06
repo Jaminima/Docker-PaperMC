6ee92dbcd94dc926930d61cd49072f9293940413
Oscar Davies <oscar@designlogica.co.uk>
2024-10-06 08:58:17 +0100

    Modify MOTD and such

--- docker-start.sh
+++ docker-start.sh
@@ -3,10 +3,10 @@
 cd /minecraft-init
 
 # Copy server folders to the server directory
-cp -r ./cache /minecraft
-cp -r ./libraries /minecraft
-cp -r ./plugins /minecraft
-cp -r ./versions /minecraft
+cp -r -f ./cache /minecraft
+cp -r -f ./libraries /minecraft
+cp -r -f ./plugins /minecraft
+cp -r -f ./versions /minecraft
 
 # Copy the server files
 cp ./eula.txt /minecraft
@@ -15,4 +15,5 @@ cp ./server-icon.png /minecraft
 
 # Start the server
 cd /minecraft
-java -Xms4G -Xmx7G -jar paper.jar --nogui
+java -Xms7G -Xmx7G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper.jar --nogui
+
